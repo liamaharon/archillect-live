059a4ecf27bc75b9593a2ee0737e99f4572573a4
liamaharon <liam.aharon@hotmail.com>
2020-06-23 09:12:24 +1000

    v0.1.0 - it works!

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
+const fs = require('fs')
+const open = require('open')
 const axios = require('axios')
+const querystring = require('querystring')
+
+const API_KEY = process.env.TWITTER_API_KEY
+const API_SECRET_KEY = process.env.TWITTER_API_SECRET_KEY
+
+let token = null
+let lastPostTime = null
 
 async function authenticate() {
-  await axios.post('api.twitter.com/oauth2/token HTTP/1.1')
+  console.log('Authenticating')
+  try {
+    const authRes = await axios({
+      method: 'post',
+      url: 'https://api.twitter.com/oauth2/token',
+      auth: {
+        username: API_KEY,
+        password: API_SECRET_KEY,
+      },
+      data: querystring.stringify({ grant_type: 'client_credentials' }),
+    })
+
+    token = authRes.data.access_token
+    console.log('Authenticated')
+    setTimeout(authenticate, 1000 * 60 * 60)
+  } catch (error) {
+    console.log('Failed to authenticate')
+    console.log(error)
+  }
 }
 
 async function updatePost() {
-  setTimeout(updatePost, 10000)
+  try {
+    const latestPostRes = await axios({
+      method: 'get',
+      url: 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=archillect&count=1',
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    const thisPostTime = latestPostRes.data[0].created_at
+    if (thisPostTime !== lastPostTime) {
+      lastPostTime = thisPostTime
+      console.log('Updating!')
+      const mediaUrl = latestPostRes.data[0].entities.media[0].media_url_https
+      const mediaType = latestPostRes.data[0].entities.media[0].type
+      const fileName = mediaType === 'photo' ? 'latest.jpg' : 'latest.gif'
+      await axios({
+        method: 'get',
+        url: mediaUrl,
+        responseType: 'stream',
+      }).then((response) => {
+        response.data.pipe(fs.createWriteStream(`${__dirname}/${fileName}`))
+      })
+      await open(`${__dirname}/${fileName}`)
+    }
+  } catch (error) {
+    console.log('Failed to fetch latest post')
+    console.log(error)
+  } finally {
+    setTimeout(updatePost, 1000 * 10)
+  }
 }
 
 async function main() {
-  console.log('Authenticating')
   await authenticate()
   await updatePost()
 }
